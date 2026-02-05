f9afa946be979ac1bc7442bf7f08b673d91c39da
Shriram Vibhute <shriram.vibhute08@gmail.com>
2026-02-05 15:20:07 +0530

    Added EC2 deployment of docker container through CICD pipeline.

--- app/app.py
+++ app/app.py
@@ -77,4 +77,4 @@ def predict():
     return render_template('index.html', result=result[0])
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000) # NOTE: Need to add this 
